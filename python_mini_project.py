cefbd799c59641a9fe92fae1b1bde63e4f4a041e
Your Name <you@example.com>
2026-06-05 16:22:08 +0530

    Update voter registration code with bug fixes

--- python_mini_project.py
+++ python_mini_project.py
@@ -1,7 +1,10 @@
 print("nishazareentaj")
+allvoterss=[]
 for i in range(5):
     age = int(input("enter your age: "))
     name = str(input("enter your name: "))
+    aadhar = input("Enter your Aadhar number: ")
+    location = input("Enter your location: ")
     if age >= 18:
         print(f"{name} eligible to vote")
         parties = ["YSR CONGRESS PARTY", "INDIAN NATIONAL CONGRESS PARTY", "TELUGHUDHESHAM PARTY", "JANASENA PARTY", "BJP", "Other", "NOTA"]
@@ -16,6 +19,27 @@ for i in range(5):
         else:
             print("Invalid choice, no party selected")
         print()  # Empty line for spacing
-            
+        leader_name=input('\n enter your favourite leader name:')
+        leader_qualities=input("enter qualities using comma:")
+        qualitieslist=[q.strip() for q in leader_qualities.split(",")]
+        print("\nOpinion Section:")
+        evm_opinion = input("Do you think EVM manipulation happened in 2024 elections? (yes/no): ").lower()
+        if evm_opinion == "yes":
+            resign_opinion = input("Do you think the present chandhrababunaidu and narendramodi should resign? (yes/no): ").lower()
+            if resign_opinion == "yes":
+                print("\nPeople is govt and govt is people, fight for your valid vote\n")
+            else:
+                print(input(list("\nso,you like present govt qualities,enter your govt benefits:")))
+        else:
+            print("\nSo, you trust the election process and the current government.\n")
+        user_data={
+            "age":str(age),
+            "name":name,
+            "party":selected_party,
+            "leader":leader_name,
+            "qualities":qualitieslist
+        }
+        print(f"\nStored details:{user_data}")
     else:
         print(f"{name} not eligible to vote\n")
+       
